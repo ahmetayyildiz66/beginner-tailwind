e2a4a5589fef400622a5811de973a1b1c02a7a37
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-06-03 15:09:43 +0300

    Custom Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,21 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    // overwrite tailwind classes
     extend: {
+      // add on to the tailwind classes
       colors: {
         orange: colors.orange,
         teal: colors.teal
+      },
+      spacing: {
+        sm: '8px',
+        md: '12px',
+        lg: '16px',
+        xl: '24px'
+      },
+      fontFamily: {
+        fun: ['Varela Round', 'sans-serif']
       }
     },
   },
